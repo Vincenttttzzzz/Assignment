ff536abf881ca064b8fec9eb272ce46a39856994
Vincent <s10257433@connect.np.edu.sg>
2023-11-30 23:39:45 +0800

    Confirmation page (Page 4) and Responsive

--- Payment.js
+++ Payment.js
@@ -4,21 +4,59 @@ const closepage = document.querySelector(".Confirm");
 
 booknow.addEventListener("click", function () {
   page.showModal();
+  storeInput();
+  getData();
 });
+
+const information = [
+  "First Name",
+  "Last Name",
+  "Email",
+  "Member",
+  "Country",
+  "Address",
+  "City",
+  "State",
+  "Card Number",
+  "Zip Code",
+  "Expiration Month",
+  "Expiration Year",
+];
 
 closepage.addEventListener("click", function () {
   page.close();
 });
 
-
-
-booknow.addEventListener("click", storeInput);
-
 function storeInput() {
-    const forms = document.querySelectorAll("form");
-    foreach (form in forms){
-        Array.from(form.elements).forEach((input) => {
-            
-        })
+  const labels = document.querySelectorAll("label");
+  const inputs = document.querySelectorAll("input");
+  const obj = {};
+
+  for (let i = 0; i < labels.length; i++) {
+    if (inputs[i].id !== null) {
+      obj[labels[i].id] = inputs[i].value;
     }
+  }
+
+  localStorage.setItem("formData", JSON.stringify(obj));
+}
+
+function getData() {
+  const formdataString = localStorage.getItem("formData");
+  const formdata = JSON.parse(formdataString);
+  let content = document.querySelector(".Confirmation-body");
+
+  let values = Object.values(formdata);
+
+  content.innerHTML = "";
+
+  for (let i = 0; i < information.length; i++) {
+    const info1 = information[i];
+    const info2 = values[i];
+    content.innerHTML += `<div class="Info"> 
+                          <span>${info1}</span>
+                          <span>${info2}</span> 
+                          </div>`;
+    console.log(localStorage);
+  }
 }
